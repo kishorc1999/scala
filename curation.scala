52946b2ec1185ebec99f5ba13ee204c99addbed4
kishorc1999 <59793145+kishorc1999@users.noreply.github.com>
2022-04-12 16:30:46 +0530

    Updated code

--- curation.scala
+++ curation.scala
@@ -6,4 +6,4 @@ import org.apache.spark.sql.SaveMode
 
 val almaren = Almaren("Demo")
 
-val df3=almaren.builder.sourceJdbc("jdbc:postgresql://w3.training5.modak.com/training_2021","org.postgresql.Driver","select * from mt3061.testartifact",Some("mt3061"),Some("mt3061@m07y21")).targetJdbc("jdbc:postgresql://w3.training5.modak.com/training_2021","org.postgresql.Driver", "mt3061.testartifact1", SaveMode.Overwrite, Some("mt3061"),Some("mt3061@m07y21"),Map("batchsize"->"5000")).batch
+val df3=almaren.builder.sourceJdbc("jdbc:postgresql://w3.training5.modak.com/training_2021","org.postgresql.Driver","select * from mt3061.testartifact",Some("mt3061"),Some("mt3061@m07y21")).targetJdbc("jdbc:postgresql://w3.training5.modak.com/training_2021","org.postgresql.Driver", "mt3061.testartifact2", SaveMode.Append, Some("mt3061"),Some("mt3061@m07y21"),Map("batchsize"->"5000")).batch
